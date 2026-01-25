97e84eb67c7b8624f7e8a9bc2610188896136dbd
Animesh Khandelwal <animeshk.2004@gmail.com>
2026-01-25 13:13:06 +0530

    Add files via upload
    Spell Bob
    
    Chef likes to play with cards a lot. Today, he's playing a game with three cards. Each card has a letter written on the top face and another (possibly identical) letter written on the bottom face. Chef can arbitrarily reorder the cards and/or flip any of the cards in any way he wishes (in particular, he can leave the cards as they were). He wants to do it in such a way that the letters on the top faces of the cards, read left to right, would spell out the name of his favorite friend Bob.
    
    Determine whether it is possible for Chef to spell "bob" with these cards.
    
    Input
    The first line of the input contains a single integer 
    T
    T denoting the number of test cases. The description of 
    T
    T test cases follows.
    The first line of each test case contains a single string with length 
    3
    3 denoting the characters written on the top faces of the first, second and third card.
    The second line contains a single string with length 
    3
    3 denoting the characters written on the bottom faces of the first, second and third card.
    Output
    For each test case, print a single line containing the string "yes" (without quotes) if Chef can spell "bob" or "no" (without quotes) if he cannot.
    
    Constraints
    1
    ≤
    T
    ≤
    20
    ,
    000
    1≤T≤20,000
    each string contains only lowercase English letters
    
    Subtasks
    Subtask #1 (100 points): original constraints

--- 1283.cpp
+++ 1283.cpp
@@ -1,21 +1,16 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    string s;
-	    cin>>s;
-	    string a="cefh";
-	    int count=0;
-	    for(int i=0;i<s.size();i++){
-	        string str=s.substr(i,4);
-	        sort(str.begin(),str.end());
-	        if(str==a)count++;
-	    }
-	    if(count==0)cout<<"normal"<<endl;
-	    else cout<<"lovely "<<count<<endl;
-	}
-    return 0;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string a,b;
+        int c=0,d=0;
+        cin>>a>>b;
+        if((a[0]=='b'||b[0]=='b')&&(a[1]=='o'||b[1]=='o')&&(a[2]=='b'||b[2]=='b')) cout<<"yes"<<endl;
+        else if((a[0]=='o'||b[0]=='o')&&(a[1]=='b'||b[1]=='b')&&(a[2]=='b'||b[2]=='b')) cout<<"yes"<<endl;
+        else if((a[0]=='b'||b[0]=='b')&&(a[1]=='b'||b[1]=='b')&&(a[2]=='o'||b[2]=='o')) cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+    }
+    
 }
